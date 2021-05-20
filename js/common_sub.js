af2d2af9ce9d0e425f85f29e952308746a519773
dbsk1111 <dbsk1111@gmail.com>
2021-05-21 01:01:14 +0900

    210521

--- js/common_sub.js
+++ js/common_sub.js
@@ -1,14 +1,17 @@
 $(function(){
-  const addHeight = 200;
-  let wHeight = $(window).innerHeight();
+  // const addHeight = 200;
+
+  //차이, scrollTop이 0일때에도 반응하느냐, 안 하느냐
 
   // 함수 따로 만들어서 호출
   activeFunc();
   function activeFunc(){
+    let wHeight = $(window).innerHeight();
+    let addHeight = wHeight*0.2;
     let thisScrollTop = $(this).scrollTop();
     $(".ts").each(function(){
       let thisOffset = $(this).offset();
-      if( thisOffset.top <= (thisScrollTop + wHeight - addHeight) && thisScrollTop < thisOffset.top ){
+      if( thisOffset.top <= (thisScrollTop + wHeight + addHeight) && thisScrollTop < thisOffset.top ){
         $(this).addClass("active");
       }
     })
@@ -19,17 +22,26 @@ $(function(){
 
   // 스크롤 메서드 바로 활용
   // $(window).scroll(function(){
+  //   let wHeight = $(window).innerHeight();
+  //   let addHeight = wHeight*0.2;
   //   let thisScrollTop = $(this).scrollTop();
   //   $(".ts").each(function(){
   //     let thisOffset = $(this).offset();
-  //     if( thisOffset.top <= (thisScrollTop + wHeight - addHeight) && thisScrollTop < thisOffset.top ){
+  //     if( thisOffset.top <= (thisScrollTop + wHeight + addHeight) && thisScrollTop < thisOffset.top ){
   //       $(this).addClass("active");
   //     }
   //   })
   // })
 
-
+  // 로컬 메뉴 노출
   $('.local_menu > ul > li').click(function(){
-    $(this).hasClass('on')
+    $(this).find('.local_submenu').stop()
+    if( $(this).hasClass('on') == true ){
+      $(this).removeClass('on')
+      $(this).find('.local_submenu').slideUp('fast')
+    }else{
+      $(this).addClass('on')
+      $(this).find('.local_submenu').slideDown()
+    }
   })
 })
